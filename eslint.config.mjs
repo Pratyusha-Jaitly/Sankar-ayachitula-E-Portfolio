80fc43bb4514f27196c58ed224c5e76af8fb8d9d
sankarayachitula <ayachitula.s@northeastern.edu>
2026-01-27 02:58:40 -0500

    Added eslint

---
+++ eslint.config.mjs
@@ -0,0 +1,14 @@
+import js from "@eslint/js";
+import globals from "globals";
+
+export default [
+  js.configs.recommended,
+  {
+    files: ["**/*.{js,mjs,cjs}"],
+    languageOptions: {
+      globals: globals.browser,
+      ecmaVersion: "latest",
+      sourceType: "module",
+    },
+  },
+];
