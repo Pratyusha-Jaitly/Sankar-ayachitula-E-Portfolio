2a69b034b0766043a8b45ee885c8d18950fbc911
pratyusha <prat.yusha.the.cool@gmail.com>
2026-02-03 18:37:40 -0500

    Fix warnings for deprected package

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -4,6 +4,9 @@ import globals from "globals";
 export default [
   js.configs.recommended,
   {
+    ignores: ["node_modules/**", "dist/**"],
+  },
+  {
     files: ["**/*.{js,mjs,cjs}"],
     languageOptions: {
       globals: globals.browser,
